a33a903cee3053ba37ddd8516c28d7b848797741
Bruno Silva <brunosilvabrait@gmail.com>
2024-01-03 15:34:26 -0300

    Database connection, category list

---
+++ src/controller/categoriesController.js
@@ -0,0 +1,13 @@
+const knex = require("../database");
+
+listCategories = async (req, res) => {
+	try {
+		const categories = await knex("categories").select("description");
+
+		return res.status(200).json(categories);
+	} catch (error) {
+		return res.status(500).json({ message: "Erro interno no servidor." });
+	}
+};
+
+module.exports = { listCategories };
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const category = require("./controller/categoriesController");
 
 const routes = express();
+
+routes.get("/categories", category.listCategories);
 
 module.exports = routes;

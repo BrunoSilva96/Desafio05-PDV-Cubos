2d6cefb4d0dc283cf3f492bdbee2875cb88fe027
Bruno Silva <brunosilvabrait@gmail.com>
2024-01-04 16:58:21 -0300

    Joi validates

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -1,9 +1,18 @@
 const Joi = require("joi");
 
 const userSchema = Joi.object({
-	nome: Joi.string().required(),
-	email: Joi.string().email().required(),
-	senha: Joi.string().min(5).required()
+	nome: Joi.string().required().messages({
+		"any.required": "O campo senha é obrigatório.",
+		"string.base": "O campo nome tem que ser uma string."
+	}),
+	email: Joi.string().email().required().messages({
+		"any.required": "O campo senha é obrigatório.",
+		"string.email": "O campo eemail deve conter um formato válido."
+	}),
+	senha: Joi.string().min(5).required().messages({
+		"any.required": "O campo senha é obrigatório.",
+		"string.min": "A senha deve conter, no mínimo 5 caracteres."
+	})
 });
 
 module.exports = userSchema;

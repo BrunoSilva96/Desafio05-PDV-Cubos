466231fd1d0cdd68934cbfeefe9e3551e9bd3fb6
Bruno Silva <brunosilvabrait@gmail.com>
2024-01-04 17:01:16 -0300

    Fix variables

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -1,16 +1,18 @@
 const Joi = require("joi");
 
 const userSchema = Joi.object({
-	nome: Joi.string().required().messages({
-		"any.required": "O campo senha é obrigatório.",
+	name: Joi.string().required().messages({
+		"any.required": "O campo nome é obrigatório.",
+		"string.empty": "O campo nome é obrigatório.",
 		"string.base": "O campo nome tem que ser uma string."
 	}),
 	email: Joi.string().email().required().messages({
-		"any.required": "O campo senha é obrigatório.",
-		"string.email": "O campo eemail deve conter um formato válido."
+		"any.required": "O campo e-mail é obrigatório.",
+		"string.email": "O campo e-mail deve conter um formato válido."
 	}),
-	senha: Joi.string().min(5).required().messages({
+	password: Joi.string().min(5).required().messages({
 		"any.required": "O campo senha é obrigatório.",
+		"string.empty": "O campo senha é obrigatório.",
 		"string.min": "A senha deve conter, no mínimo 5 caracteres."
 	})
 });

dc056aeb03d4ec611605cd5503385c260437bd4a
Bruno Silva <brunosilvabrait@gmail.com>
2024-01-08 17:37:45 -0300

    Adding product schema, pending refactoring

---
+++ src/schemas/productSchema.js
@@ -0,0 +1,19 @@
+const Joi = require("joi");
+
+const productSchema = Joi.object({
+	description: Joi.string().required().messages({
+		"any.required": "O campo descrição é obrigatório.",
+		"string.empty": "O campo descrição é obrigatório.",
+		"string.base": "O campo descrição tem que ser uma string."
+	}),
+	stock_quantity: Joi.number().email().required().messages({
+		"any.required": "O campo quantidade de estoque é obrigatório."
+	}),
+	value: Joi.number().required().messages({
+		"any.required": "O campo valor é obrigatório.",
+		"string.empty": "O campo valor é obrigatório."
+	}),
+	category_id: Joi.number().required().messages({
+		"any.required": "O campo id da categoria é obrigatório."
+	})
+});

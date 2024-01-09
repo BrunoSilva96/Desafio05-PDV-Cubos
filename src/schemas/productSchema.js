11c0129ece152eb549730b7f6fee5012de42d677
Bruno Silva <brunosilvabrait@gmail.com>
2024-01-09 14:26:48 -0300

    Modified product schema

--- src/schemas/productSchema.js
+++ src/schemas/productSchema.js
@@ -7,14 +7,16 @@ const productSchema = Joi.object({
 		"string.base": "O campo descrição tem que ser uma string."
 	}),
 	stock_quantity: Joi.number().required().messages({
-		"any.required": "O campo quantidade de estoque é obrigatório."
+		"any.required": "O campo quantidade de estoque é obrigatório.",
+		"number.base": "A quantidade de estoque deve ser um número."
 	}),
 	value: Joi.number().required().messages({
 		"any.required": "O campo valor é obrigatório.",
-		"string.empty": "O campo valor é obrigatório."
+		"number.base": "O valor deve ser um número."
 	}),
 	category_id: Joi.number().required().messages({
-		"any.required": "O campo id da categoria é obrigatório."
+		"any.required": "O campo id da categoria é obrigatório.",
+		"number.base": "O id deve ser um número."
 	})
 });
 
